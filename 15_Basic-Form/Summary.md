b654927548f108b2934239a9326401a0f885790d
Aldi Saputra Rahmawan <11191007@student.itk.ac.id>
2022-04-01 02:18:48 +0800

    Finished react hook

---
+++ 15_Basic-Form/Summary.md
@@ -0,0 +1,16 @@
+Form merupakan salah satu hal krusial dalam pengembangan aplikasi website. Form dapat digunakan untuk menghandle inputan dari user.
+
+Form banyak kita jumpai saat login, mendaftarkan sesuatu, memberikan feedback, mengisi data dan masih banyak lainnya.
+
+Tipe Form :
+- Elemen <input>
+- Elemen <textarea>
+- Elemen <select>
+- Radio Button
+- Checkbox
+
+Kita dapat menggabungkan cara menyimpan dan memperbarui state di HTML dan React dengan menggunakan state pada React. Kemudian Komponen React yang me-render sebuah form juga mengontrol apa yang terjadi dalam form tersebut pada masukan pengguna selanjutnya.
+
+Sebuah elemen masukan form yang nilainya dikontrol oleh React melalui cara seperti ini disebut sebagai ”controlled component“.
+
+Uncontrolled component adalah alternatif lain dari controlled component, dimana data form akan ditangani oleh DOM-nya sendiri. Untuk menulis uncontrolled component, alih-alih menulis event handler untuk setiap pembaruan state, kita bisa menggunakan ref untuk mendapatkan nilai form dari DOM.
--- 15_Basic-Form/praktikum/src/components/pages/Body.jsx
+++ 15_Basic-Form/praktikum/src/components/pages/Body.jsx
@@ -1,44 +1,181 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './css/body.css'
 import {Container} from 'react-bootstrap'
-import {useForm} from 'react-hook-form';
+
+
 
 export default function Body(props) {
-    const {register, handleSubmit, errors} = useForm()
+    const mockData = {
+        nama:"",
+        email:"",
+        noTelepon:"",
+        pendidikan:"",
+        kelas:"",
+        harapan:""
+    }
+
+    const inputErrors = {
+        nama:"",
+        email:"",
+        noTelepon:"",
+    }
+    
+    const [data, setData] = useState(mockData);
+    const [message, setMessage] = useState(inputErrors);
+    const regexName = /^[a-zA-Z\s]*$/;
+    const regexNoTelepon = /^[0-9]*$/;
+    const regexEmail = /^\w+@[A-Za-z_]+?\.[a-zA-Z]{2,3}$/;
+    const [IT, setIT] = useState(false);
+    const [nonIT, setNonIT] = useState(false);
+    const suratKesungguhan = useRef(null);
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setData({
+            ...data,
+            [name]:value
+        })
+
+        if (name === "nama"){
+            if(!regexName.test(value)){
+                // console.log("nama harus berupa huruf!!")
+                setMessage({...message, nama: "Nama harus berupa huruf!!!"})
+            }
+            else{
+                setMessage("")
+            }
+        }
+        if (name === "email"){
+            if(!regexEmail.test(value)){
+                setMessage({...message, email: "Email yang anda masukkan tidak sesuai format!!"})
+            }
+            else{
+                setMessage("")
+            }
+        }
+
+        if (name === "noTelepon"){
+            if(!regexNoTelepon.test(value)){
+                setMessage({...message, noTelepon: "Harus berupa angka!!"})
+            }
+            else{
+                setMessage("")
+            }
+        }
+
+        console.log("isi data form", data)
+    }
   
+    const handleSubmit = (e) => {
+        if (message === ""){
+            alert("Berhasil Mendaftar")
+        }
+        else {
+            alert("Data Tidak Sesuai Format")
+        }
+    }
+
+    const handleSelect = (e) => {
+        setData({
+            ...data,
+            kelas: e.target.value
+        })
+        // console.log(data)
+    }
+
+    const resetData = (e) => {
+        setData(mockData);
+        setMessage("");
+    }
+
+    const radioChecked = (e) => {
+        if(e.target.value === "IT"){
+            setIT(!IT);
+            setNonIT(false);
+            setData({
+               ...data,
+               pendidikan:"IT" 
+            })
+        }
+        else if(e.target.value === "NonIT"){
+            setNonIT(!nonIT);
+            setIT(false);
+            setData({
+                ...data,
+                pendidikan:"NonIT"
+            })
+        }
+    }
+
+
+
     return (
-    <div className='form'>
-        <form>
+    <div className='form' >
+        <form onSubmit={handleSubmit}>
             <Container>
                 <label>
                     Nama Lengkap :
-                    <input className="biodata" type="text" placeholder="Masukkan nama anda" required/>
+                    <input 
+                    className="biodata" 
+                    name="nama" 
+                    value={data.nama} 
+                    type="text" 
+                    placeholder="Masukkan nama anda" 
+                    onChange={handleChange} 
+                    required/>
+                {message.nama && <p style={{width: "100%", color: "red"}}>{message.nama}</p>}
                 </label>
                 <label>
                     Email :
-                    <input className="biodata" type="email" placeholder="Masukkan email anda" required/>
+                    <input 
+                    className="biodata" 
+                    name="email" 
+                    value={data.email} 
+                    type="email" 
+                    placeholder="Masukkan email anda" 
+                    onChange={handleChange}
+                    required/>
+                {message.email && <p style={{width: "100%", color: "red"}}>{message.email}</p>}
                 </label>
                 <label>
                     No Telp :
                     <input 
                     className="biodata" 
+                    name="noTelepon"
+                    value={data.noTelepon}
                     type="text" 
-                    laceholder="Masukkan nomor telepon anda" 
-                    minlength="9" 
-                    maxlength="14" 
+                    placeholder="Masukkan nomor telepon anda" 
+                    minLength="9"
+                    maxLength="14"
+                    onChange={handleChange}
                     required/>
+                {message.noTelepon && <p style={{width: "100%", color: "red"}}>{message.noTelepon}</p>}
                 </label>
             </Container>   
             <label className='label-btn' required>
                 Latar Belakang Pendidikan :<br></br>
-                <input id="IT" type="radio" value="IT" name="IT"/>
+                <input 
+                type="radio" 
+                value="IT" 
+                name="pendidikan" 
+                checked={IT}
+                onChange={handleChange}
+                onClick={radioChecked} />
                 <label htmlFor="IT">IT</label>
-                <input id="Non-IT" type="radio" value="Non IT" name="Non IT"/>
-                <label htmlFor="Non-IT">Non IT</label>
+                <input 
+                type="radio" 
+                value="NonIT" 
+                name="pendidikan"
+                checked={nonIT}
+                onChange={handleChange}
+                onClick={radioChecked}/>
+                <label htmlFor="NonIT">Non IT</label>
             </label>
             <Container>
                 <label htmlFor='kelas-coding'>Kelas Koding yang Dipilih :
-                    <select className='biodata' name='kelas' id='kelas' required>
+                    <select className='biodata' name='kelas' id='kelas' onChange={handleSelect} required>
                         <option value='Default'>Pilih Salah Satu Program</option>
                         <option value='Golang'>Coding Backend with Golang</option>
                         <option value='ReactJS'>Coding Frontend with ReactJS</option>
@@ -47,15 +184,27 @@ export default function Body(props) {
                 </label>
                 <label>
                     Foto Surat Kesungguhan :
-                    <input className='biodata' type="file" id='userFile' name='filename' required/>
+                    <input 
+                    className='biodata' 
+                    type="file" 
+                    id='userFile' 
+                    name='filename'
+                    ref={suratKesungguhan}
+                    required/>
                 </label>
                 <label>
                     Harapan Untuk Coding Bootcamp Ini :
-                    <textarea className="biodata" id='userHope' name='hope' rows="4" cols="50"/>
+                    <textarea 
+                    className="biodata" 
+                    name='harapan' 
+                    rows="4" 
+                    cols="50"
+                    value={data.harapan}
+                    onChange={handleChange}/>
                 </label>
                 <div>
-                    <button className='submit-btn'>Submit</button>
-                    <button className='reset-btn'>Reset</button>
+                    <button className='submitbtn' value="submit">Submit</button>
+                    <button className='resetbtn' onClick={resetData}>Reset</button>
                 </div>
             </Container>
         </form>
